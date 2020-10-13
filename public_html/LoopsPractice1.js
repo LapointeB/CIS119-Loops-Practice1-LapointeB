95e386d76421cabbe28adca7b487989445bb4d41
LapointeB <LapointeB@T29685.niaccit.niacc.edu>
2020-10-13 09:36:28 -0500

    Did the program

--- public_html/LoopsPractice1.js
+++ public_html/LoopsPractice1.js
@@ -1,6 +1,13 @@
 /* 
  * Display the numbers from 1 to 10
  */
+let counter = 1;
 
 document.write("The numbers from 1 to 10 are: <br>");
+
+while (counter <= 10) {
+    document.write(counter + "<br>");
+    counter = counter + 1;
+}
+
 
